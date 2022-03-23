990c9d859ed2fe9831dcaf792aaf908c91d71855
Duong-Xuan-Long <linesco218@gmail.com>
2022-03-23 10:03:15 +0700

    BTVNlesson1

---
+++ Lesson_1/BTVN/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a=2;
+    int b=3;
+        System.out.println("a+b="+(a+b));
+        System.out.println("a-b="+(a-b));
+        System.out.println("b-a="+(b-a));
+        System.out.println("a*b="+(a*b));
+        System.out.println("a/b="+((double)a/b));
+        System.out.println("b/a="+((double)b/a));
+    }
+}
