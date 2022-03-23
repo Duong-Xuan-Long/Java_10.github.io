10f861481aa34e45b703afe506d7062fdc40a9b1
Duong-Xuan-Long <linesco218@gmail.com>
2022-03-23 20:31:20 +0700

    github

--- Lesson_1/BTVN/src/com/company/Main.java
+++ Lesson_1/BTVN/src/com/company/Main.java
@@ -5,11 +5,13 @@ public class Main {
     public static void main(String[] args) {
 	int a=2;
     int b=3;
+
         System.out.println("a+b="+(a+b));
         System.out.println("a-b="+(a-b));
         System.out.println("b-a="+(b-a));
         System.out.println("a*b="+(a*b));
         System.out.println("a/b="+((double)a/b));
         System.out.println("b/a="+((double)b/a));
+
     }
 }
---
+++ Lesson_1/EXPERIMENT/src/com/company/Main.java
@@ -0,0 +1,152 @@
+package com.company;
+//import java.util.Scanner;
+//import javax.swing.JOptionPane;
+//import java.util.Scanner;
+//import java.util.Random;
+
+//import java.util.ArrayList;
+//import java.util.ArrayList;
+//import java.util.Scanner;
+//import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Scanner scanner=new Scanner(System.in);
+//	String name=JOptionPane.showInputDialog("Enter your name");
+//    JOptionPane.showMessageDialog(null,"Hello "+name);
+//    int age=Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+//        JOptionPane.showMessageDialog(null,"you are " +age+" years old");
+//    double height=Double.parseDouble(JOptionPane.showInputDialog("Enter your height :"));
+//    JOptionPane.showMessageDialog(null,"you are "+height+" cm in height");
+//        System.out.println("what is your name : ");
+//        String name=scanner.nextLine();
+//        System.out.println("your name is "+name);
+//        System.out.println("what is your age: ");
+//        int age=scanner.nextInt();
+//        System.out.println("your age is "+age);
+//        scanner.nextLine();
+//        System.out.println("what is your favorite food : ");
+//        String food=scanner.nextLine();
+//        System.out.println("your favorite food is "+ food);
+
+//Scanner scanner=new Scanner(System.in);
+//double x;
+//double y;
+//double z;
+//        System.out.println("Enter side x : ");
+//        x=scanner.nextDouble();
+//        System.out.println("Enter side y :");
+//        y=scanner.nextDouble();
+//        z=Math.sqrt((x*x)+(y*y));
+//        System.out.println("the hypotenuse of the pyramid is "+z);
+//        scanner.close();
+//        Random random=new Random();
+//        int x=random.nextInt(2)+1;
+//        System.out.println(x);
+//        int age=80;
+//        if(age>=75){
+//            System.out.println(" ok! boomer");
+//        }
+//        else if(age>=13){
+//            System.out.println("you are a teenager");
+//        }
+//        String day="Moday";
+//        switch(day){
+//            case "Monday":
+//                System.out.println("it is Monday");
+//                break;
+//            case "tuseday":
+//                System.out.println("it is Tuesday");
+//                break;
+//            default:
+//                System.out.println("that is not a day");
+//        }
+//        String name="";
+//        Scanner scanner =new Scanner(System.in);
+//
+//        while(name.isBlank()){
+//            System.out.println("enter your name");
+//            name=scanner.nextLine();
+//        }
+//
+//        System.out.println("hello "+ name);
+//        for(int i=10;i>=0;i--) {
+//            System.out.println(i);
+//        }
+//        System.out.println("Hello world");
+//        Scanner scanner =new Scanner(System.in);
+//        int rows;
+//        int columns;
+//        String symbol="";
+//        System.out.println("enter rows ");
+//        rows=scanner.nextInt();
+//        System.out.println("enter columns ");
+//        columns=scanner.nextInt();
+//        System.out.println("enter symbol ");
+//        symbol=scanner.next();
+//        for(int i=1;i<=rows;i++){
+//            System.out.println();
+//            for(int j=1;j<=columns;j++){
+//                System.out.print(symbol);
+//            }
+//        }
+//        String[] cars={"camaro","tesla","huynhdai"};
+//        System.out.println(cars);
+//        String[] cars=  new String[3];
+//        cars[0]="camaro";
+//        cars[1]="toyota";
+//        cars[2]="tesla";
+////        System.out.println(cars[0]);
+//        for(int i=0;i<cars.length;i++){
+//            System.out.println(cars[i]);
+//        }
+//          String[][] cars = {{"a","b","c"},{"d","e","f"}};
+////          cars[0][0]="a";
+////          cars[0][1]="b";
+////        cars[0][2]="c";
+////        cars[1][0]="d";
+////        cars[1][1]="e";
+////        cars[1][2]="f";
+//        for(int i=0;i<cars.length;i++){
+//            System.out.println();
+//            for(int j=0;j<cars[i].length;j++){
+//                System.out.print(cars[i][j]+" ");
+//            }
+//
+//        }
+//  ArrayList<String> food= new ArrayList<String>();
+//  food.add("pizza");
+//  food.add("fish");
+//  food.add("cat");
+//  food.set(0,"sushi");
+//  food.remove(0);
+//  food.clear();
+//  for(int i=0;i<food.size();i++){
+//      System.out.println(food.get(i));
+//  }
+//        ArrayList<ArrayList<String>> groceryList= new ArrayList<>();
+//        ArrayList<String> bakeryList= new ArrayList<>();
+//        bakeryList.add("pasta");
+//        bakeryList.add("garlic bread");
+//        bakeryList.add("donut");
+//        System.out.println(bakeryList);
+//
+//        ArrayList<String> drinkList= new ArrayList<>();
+//        drinkList.add("coca");
+//        drinkList.add("soda");
+//        drinkList.add("orange juice");
+//
+//        ArrayList<String> produceList= new ArrayList<>();
+//        produceList.add("butter");
+//        produceList.add("tomatoes");
+//
+//        groceryList.add(bakeryList);
+//        groceryList.add(produceList);
+//        groceryList.add(drinkList);
+//
+//        System.out.println(groceryList.get(0).get(2));
+//
+    }
+
+}
