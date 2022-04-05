e381c7acb9661ed547edad3f38ad3a05b4b7f571
Duong-Xuan-Long <linesco218@gmail.com>
2022-04-05 21:28:51 +0700

    okla

--- Exercise_6/src/com/company/Main.java
+++ Exercise_6/src/com/company/Main.java
@@ -1,14 +1,44 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
-	Scanner scanner=new Scanner(System.in);
-    String email=scanner.nextLine();
-    long phoneNumber= scanner.nextLong();
-    if(Pattern.matches("@gmail.com"{1}))
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhập email : ");
+        String email = scanner.nextLine();
+
+//        long phoneNumber = scanner.nextLong();
+        String regex = "^\\w+[a-z0-9]*@{1}\\w+mail.com$";
+        Pattern pattern= Pattern.compile(regex);
+        Matcher matcher=pattern.matcher(email);
+        if(matcher.find()){
+            System.out.println("valid");
+
+        }
+        else{
+            System.out.println("invalid");
+        }
+        System.out.println("nhập số điện thoại : ");
+        String phonenumber = scanner.nextLine();
+        scanner.close();
+        String regex1 = "^0[98]{1}\\d{7,9}$";
+        Pattern pattern1= Pattern.compile(regex1);
+        Matcher matcher1=pattern1.matcher(phonenumber);
+        if(matcher1.find()){
+            if(phonenumber.length()==9||phonenumber.length()==11){
+                System.out.println("valid");
+            }
+           else{
+                System.out.println("invalid");
+            }
+
+        }
+        else{
+            System.out.println("invalid");
+        }
     }
 }
